e6629d07cc67355726087352db2ef879f020444b
ysnkd <kadirkol123@gmail.com>
2022-09-14 00:13:02 +0300

    Proje dosyası ekle.

---
+++ Kamp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> kurslar = new List<string> { };
+
+            kurslar.Add(".NET");
+            kurslar.Add("Java");
+            kurslar.Add("Angular");
+
+            foreach (var item in kurslar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
