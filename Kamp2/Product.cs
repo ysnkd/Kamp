32cb7c0c66a116d2fc375ebd840bca0a100a1688
ysnkd <kadirkol123@gmail.com>
2022-09-14 23:58:37 +0300

    2.

---
+++ Kamp2/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kamp2
+{
+    class Product
+    {
+        //properties
+
+        
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Kamp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Kamp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.Name = "Apple";
+            product1.Price = 25;
+
+            Product product2 = new Product();
+            product2.Id = 2;
+            product2.Name = "Banana";
+            product2.Price = 34;
+
+            Product product3 = new Product();
+            product3.Id = 3;
+            product3.Name = "Melon";
+            product3.Price = 42;
+
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            //For döngüsü
+
+           
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].Name);
+            }
+
+            //foreach 
+
+            foreach (var x in products)
+            {
+                Console.WriteLine(x.Id);
+            }
+
+            //while döngüsü
+
+            int sayac = 0;
+
+            while(sayac<products.Length)
+            {
+                Console.WriteLine(products[sayac].Price);
+                sayac++;
+                
+            }
+
+        }
+    }
+}
