cc07037770df9c498a0bbc97c13db71153d064d1
ysnkd <kadirkol123@gmail.com>
2022-09-15 00:51:14 +0300

    Odev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriler = new List<Musteri> { };
+
+        public void Add(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+        }
+
+        public void Show()
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Yasin";
+            musteri1.Soyad = "Kol";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+
+            musteriManager.Show();
+        }
+    }
+}
