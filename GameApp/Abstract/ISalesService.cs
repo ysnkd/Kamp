99f15141adaaeae33a38ebe120cebbdb240be0f7
ysnkd <80248322+ysnkd@users.noreply.github.com>
2022-09-23 00:35:52 +0300

    Odev5
    1.     Oyuncuların sisteme kayıt olabileceği, bilgilerini güncelleyebileceği, kayıtlarını silebileceği bir ortamı simule ediniz. Müşteri bilgilerinin doğruluğunu e-devlet sistemlerini kullanarak doğrulama yapmak istiyoruz. (E-devlet sistemlerinde doğrulama TcNo, Ad, Soyad, DoğumYılı bilgileriyle yapılır. Bunu yapacak servisi simule etmeniz yeterlidir.)
    
    2.     Oyun satışı yapılabilecek satış ortamını simule ediniz.( Yapılan satışlar oyuncu ile ilişkilendirilmelidir. Oyuncunun parametre olarak metotta olmasını kastediyorum.)
    
    3.     Sisteme yeni kampanya girişi, kampanyanın silinmesi ve güncellenmesi imkanlarını simule ediniz.
    
    4.     Satışlarda kampanya entegrasyonunu simule ediniz.
    
    5.     Ödevinizi Github’a yükleyiniz. Github linkinizi paylaşınız.

---
+++ GameApp/Abstract/ISalesService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameApp.Abstract
+{
+    interface ISalesService
+    {
+        void SellTo(Gamer gamer, Game game);
+    }
+}
---
+++ GameApp/Concrete/GamerManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameApp
+{
+    //MicroService
+    class GamerManager : IGamerService
+    {
+        //hiç bir class'ı çıplak bırakmayacağız. (interface)
+        //Eğer bir manager classında bir manager classı kullanacaksak onu sakın newleme!
+        IUserValidationService userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            this.userValidationService = userValidationService;
+        }
+
+        //CONSTRUCTOR ; BU ŞU DEMEK GAMERMANAGER'IN userValidationServis adında bir BAĞIMLILIĞI VAR! GAMERMANAGER SINIFINDA 
+        // BİR DOĞRULAMA KULLANACAĞIM DEMEK
+        //KULLANDIĞIMIZ CONSTRUCTOR DEĞER ALAN CONSTRUCTOR. MAİNDE GAMERMANAGER'İ NEWLEDİĞİMDE YANINDA PARANTEZ
+        //OLARAK BİR DOĞRULAMA SİSTEMİNİ YAZMALIYIZ. ZORUNDAYIZ
+        public void Add(Gamer gamer)
+        {
+            if(userValidationService.Validate(gamer))
+            {
+                Console.WriteLine(gamer.FirstName+ " kayıt oldu");
+
+            }
+            else
+            {
+                Console.WriteLine("kayıt başarısız");
+            }
+            //GAMER'İ DOĞRULADIYSAN KAYIT OLDU MESAJI VER!
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("kayıt silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("kayıt güncellendi");
+        }
+    }
+}
